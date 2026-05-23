c05024c40b85aadfeb17ad64b6b074e9b334135a
Workstation5495 <4oksana1@gmail.com>
2026-05-23 15:22:32 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,36 +1,9 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
-// Твой конфиг (просто вставь его сюда)
-const firebaseConfig = {
-    apiKey: "ТВОЙ_КЛЮЧ_ВСТАВЬ_СЮДА", 
+// js/firebase-config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyDLKq6i6ZddbYAmlcazoFjMl05f8ZL8dUc",
     authDomain: "resonatemessenger.firebaseapp.com",
     projectId: "resonatemessenger",
     storageBucket: "resonatemessenger.firebasestorage.app",
     messagingSenderId: "669195536581",
     appId: "1:669195536581:web:8f29438fefa9170b0948ac"
 };
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-
-// Кнопка входа
-const loginBtn = document.getElementById('login-btn');
-if (loginBtn) {
-    loginBtn.addEventListener('click', () => {
-        signInWithPopup(auth, provider).catch((error) => console.log(error));
-    });
-}
-
-// Проверка: вошел пользователь или нет
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        document.getElementById('auth-box').style.display = 'none';
-        document.getElementById('content').style.display = 'block';
-        console.log("Welcome, " + user.displayName);
-    } else {
-        document.getElementById('auth-box').style.display = 'block';
-        document.getElementById('content').style.display = 'none';
-    }
-});
