147b68ea1455428afdd1b7244c0b03b0ea3af5d8
Workstation5495 <4oksana1@gmail.com>
2026-05-23 15:09:48 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { firebaseConfig } from './firebase-config.js';
-console.log("Resonate Hub connected!");
+
+// Инициализация Firebase
+const app = initializeApp(firebaseConfig);
+console.log("Resonate Hub!");
+
+// Слушаем кнопку
+document.getElementById('loginBtn').addEventListener('click', () => {
+    alert("Login system coming soon!");
+});
