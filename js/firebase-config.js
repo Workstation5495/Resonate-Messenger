5629c5aba7a6d73f435dcfe693f250035293c40c
Workstation5495 <4oksana1@gmail.com>
2026-05-23 15:45:05 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,9 @@
-// js/firebase-config.js
+// Этот файл будет перезаписан сервером GitHub при деплое
 export const firebaseConfig = {
-    apiKey: "AIzaSyDLKq6i6ZddbYAmlcazoFjMl05f8ZL8dUc",
-    authDomain: "resonatemessenger.firebaseapp.com",
-    projectId: "resonatemessenger",
-    storageBucket: "resonatemessenger.firebasestorage.app",
-    messagingSenderId: "669195536581",
-    appId: "1:669195536581:web:8f29438fefa9170b0948ac"
+  apiKey: "WILL_BE_FILLED_BY_GITHUB",
+  authDomain: "resonatemessenger.firebaseapp.com",
+  projectId: "resonatemessenger",
+  storageBucket: "resonatemessenger.firebasestorage.app",
+  messagingSenderId: "669195536581",
+  appId: "1:885733250674:web:8f29438fefa9170b0948ac"
 };
